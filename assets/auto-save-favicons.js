c1d6d729cf906adba6ed6025078c1f73f9d970b5
Hamilton <Hambomyers@gmail.com>
2025-06-17 21:17:35 -0400

    Delete bloat auto-save-favicons.js file

--- assets/auto-save-favicons.js
+++
@@ -1,4 +0,0 @@
-// FAVICON AUTO-GENERATION PERMANENTLY DISABLED
-// The beautiful chiclet favicon with BZL cutout is perfect as-is
-// No automatic generation or modification allowed
-console.log('Favicon auto-generation disabled - using perfect chiclet favicon');
